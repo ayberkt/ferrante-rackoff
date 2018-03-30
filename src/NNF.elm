edcdc1a67081b56bb4087827e607e82d5832dccc
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-03-29 23:54:35 -0500

    Align things

--- src/NNF.elm
+++ src/NNF.elm
@@ -7,11 +7,11 @@ convertToNNF p =
   case p of
     Neg (Conj p1 p2) -> Disj (convertToNNF (Neg p1)) (convertToNNF (Neg p2))
     Neg (Disj p1 p2) -> Conj (convertToNNF (Neg p1)) (convertToNNF (Neg p2))
-    Neg (Neg p)        -> convertToNNF p
-    Neg (Forall p)     -> Exists (convertToNNF (Neg p))
-    Neg (Exists p)     -> Forall (convertToNNF (Neg p))
-    Conj p1 p2         -> Conj (convertToNNF p1) (convertToNNF p2)
-    Disj p1 p2         -> Disj (convertToNNF p1) (convertToNNF p2)
-    Forall p           -> Forall (convertToNNF p)
-    Exists p           -> Exists (convertToNNF p)
-    p                  -> p
+    Neg (Neg p)      -> convertToNNF p
+    Neg (Forall p)   -> Exists (convertToNNF (Neg p))
+    Neg (Exists p)   -> Forall (convertToNNF (Neg p))
+    Conj p1 p2       -> Conj (convertToNNF p1) (convertToNNF p2)
+    Disj p1 p2       -> Disj (convertToNNF p1) (convertToNNF p2)
+    Forall p         -> Forall (convertToNNF p)
+    Exists p         -> Exists (convertToNNF p)
+    p                -> p
