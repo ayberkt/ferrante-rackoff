87da9e982b52ab8ae6f11b6911070ba6997515ba
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-03-30 14:00:58 -0500

    Implement `removeAllNegations`

--- src/OmitNegations.elm
+++ src/OmitNegations.elm
@@ -1,4 +1,15 @@
-import Syntax exposing (Prop(..))
+module OmitNegations exposing (removeAllNegations)
+
+import Syntax exposing (Prop(..), RatPred(..))
 
 removeAllNegations : Prop -> Prop
-removeAllNegations p = Top -- TODO.
+removeAllNegations p =
+  case p of
+    Neg (Pred (Less e1 e2)) -> Disj (Pred (Less e2 e1))   (Pred (Eq      e1 e2))
+    Neg (Pred (Eq   e1 e2)) -> Disj (Pred (Less e1 e2))   (Pred (Greater e1 e2))
+    Neg p            -> Neg p -- this case should not happen.
+    Conj p1 p2     -> Conj   (removeAllNegations p1) (removeAllNegations p2)
+    Disj p1 p2     -> Disj   (removeAllNegations p1) (removeAllNegations p2)
+    Forall p1      -> Forall (removeAllNegations p1)
+    Exists p1      -> Exists (removeAllNegations p1)
+    p                -> p
