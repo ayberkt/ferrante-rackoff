54113a77ab8b68b89352186cac23ba52d8b473f4
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-03-30 17:34:46 -0500

    Add the negation case and run through elm-format

--- src/Syntax.elm
+++ src/Syntax.elm
@@ -1,37 +1,59 @@
 module Syntax exposing (Prop(..), RatPred(..), linearize)
 
-type Expr =
-    Zero
-  | One
-  | Plus
-  | Minus
-  | Var Int String
 
-type RatPred =
-    Eq      Expr Expr
-  | Greater Expr Expr
-  | Less    Expr Expr
+type Expr
+    = Zero
+    | One
+    | Plus
+    | Minus
+    | Var Int String
 
-type Prop =
-    Pred RatPred
-  | Top
-  | Bot
-  | Neg Prop
-  | Conj Prop Prop
-  | Disj Prop Prop
-  | Forall Prop
-  | Exists Prop
+
+type RatPred
+    = Eq Expr Expr
+    | Greater Expr Expr
+    | Less Expr Expr
+
+
+type Prop
+    = Pred RatPred
+    | Top
+    | Bot
+    | Neg Prop
+    | Conj Prop Prop
+    | Disj Prop Prop
+    | Forall Prop
+    | Exists Prop
+
 
 linearizeRatPred : RatPred -> String
-linearizeRatPred rp = "TODO"
+linearizeRatPred rp =
+    "TODO"
+
 
 linearize : Prop -> String
 linearize p =
-  case p of
-    Top           -> "\\top"
-    Bot           -> "\\bot"
-    Conj    p1 p2 -> (linearize p1) ++ "\\wedge" ++ (linearize p2)
-    Disj    p1 p2 -> (linearize p1) ++ "\\vee"   ++ (linearize p2)
-    Forall  p1    -> "\\forall.\\ " ++ (linearize p1)
-    Exists  p1    -> "\\exists.\\ " ++ (linearize p1)
-    _             -> "TODO"
+    case p of
+        Top ->
+            "⊤"
+
+        Bot ->
+            "⊥"
+
+        Conj p1 p2 ->
+            (linearize p1) ++ "∧" ++ (linearize p2)
+
+        Disj p1 p2 ->
+            (linearize p1) ++ "∨" ++ (linearize p2)
+
+        Forall p1 ->
+            "\\forall.\\ " ++ (linearize p1)
+
+        Exists p1 ->
+            "\\exists.\\ " ++ (linearize p1)
+
+        Neg p1 ->
+            "¬" ++ linearize p1
+
+        _ ->
+            "TODO"
