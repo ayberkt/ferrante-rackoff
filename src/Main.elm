690c8cbdfe6d151a4bc7144472c9e181edddd773
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-03-30 17:34:33 -0500

    Minor changes

--- src/Main.elm
+++ src/Main.elm
@@ -1,38 +1,54 @@
+module Main exposing (..)
+
 import Html exposing (Html, Attribute, beginnerProgram, text, div, input)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import String
 import Syntax exposing (Prop(..), linearize)
+import PropositionParser exposing (parseProp)
 
 
 main =
-  beginnerProgram { model = "", view = view, update = update }
+    beginnerProgram { model = "", view = view, update = update }
+
 
 
 -- UPDATE
 
-type Msg = NewContent String
+
+type Msg
+    = NewContent String
+
 
 update (NewContent content) oldContent =
-  content
+    content
+
 
 
 -- VIEW
 
-inputInterpretation s = "Input interpretation: " ++ s
+
+inputInterpretation s =
+    case parseProp s of
+        Err s ->
+            "could not parse input"
+
+        Ok p ->
+            "Input interpretation: " ++ (linearize p) ++ "."
+
 
 view content =
-  div []
-    [ input [ placeholder "Enter a proposition.", onInput NewContent, myStyle ] []
-    , div [ myStyle ] [ text (inputInterpretation content) ]
-    , div [ myStyle ] [ text (linearize (Conj Top Bot)) ]
-    ]
+    div []
+        [ input [ placeholder "Enter a proposition.", onInput NewContent, myStyle ] []
+        , div [ myStyle ] [ text (inputInterpretation content) ]
+        ]
+
 
 myStyle =
-  style
-    [ ("width", "100%")
-    , ("height", "40px")
-    , ("padding", "10px 0")
-    , ("font-size", "2em")
-    , ("text-align", "center")
-    ]
+    style
+        [ ( "width", "100%" )
+        , ( "height", "40px" )
+        , ( "padding", "10px 0" )
+        , ( "font-size", "2em" )
+        , ( "text-align", "center" )
+        ]
