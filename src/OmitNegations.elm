c094eb64133b2aa1f46620f483a4b2e65f9b48b0
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-03-31 00:36:10 -0500

    Account for variables of forall and run things through elm-format

--- src/OmitNegations.elm
+++ src/OmitNegations.elm
@@ -2,14 +2,31 @@ module OmitNegations exposing (removeAllNegations)
 
 import Syntax exposing (Prop(..), RatPred(..))
 
+
 removeAllNegations : Prop -> Prop
 removeAllNegations p =
-  case p of
-    Neg (Pred (Less e1 e2)) -> Disj (Pred (Less e2 e1))   (Pred (Eq      e1 e2))
-    Neg (Pred (Eq   e1 e2)) -> Disj (Pred (Less e1 e2))   (Pred (Greater e1 e2))
-    Neg p            -> Neg p -- this case should not happen.
-    Conj p1 p2     -> Conj   (removeAllNegations p1) (removeAllNegations p2)
-    Disj p1 p2     -> Disj   (removeAllNegations p1) (removeAllNegations p2)
-    Forall p1      -> Forall (removeAllNegations p1)
-    Exists p1      -> Exists (removeAllNegations p1)
-    p                -> p
+    case p of
+        Neg (Pred (Less e1 e2)) ->
+            Disj (Pred (Less e2 e1)) (Pred (Eq e1 e2))
+
+        Neg (Pred (Eq e1 e2)) ->
+            Disj (Pred (Less e1 e2)) (Pred (Greater e1 e2))
+
+        Neg p ->
+            Neg p
+
+        -- this case should not happen.
+        Conj p1 p2 ->
+            Conj (removeAllNegations p1) (removeAllNegations p2)
+
+        Disj p1 p2 ->
+            Disj (removeAllNegations p1) (removeAllNegations p2)
+
+        Forall s p1 ->
+            Forall s (removeAllNegations p1)
+
+        Exists p1 ->
+            Exists (removeAllNegations p1)
+
+        p ->
+            p
