74a5a859d4ff5dae6156aef6e66eff907e8fc032
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-04-16 19:03:04 -0500

    Run through elm-format

--- src/Solve.elm
+++ src/Solve.elm
@@ -2,36 +2,54 @@ module Solve exposing (solve)
 
 import Syntax exposing (Prop(..), RatPred(..), Expr(..), Rat(..))
 
+
 -- Take a proposition `p` that is assumed to be in NNF
 -- and remove all negations that are on literals.
-
 -- Negate a given rational number i.e., divide 1 by the given number.
+
+
 negateByMul : Rat -> Rat
-negateByMul (Div n1 n2) = Div n2 n1
+negateByMul (Div n1 n2) =
+    Div n2 n1
+
 
 solveRatPred : RatPred -> RatPred
 solveRatPred p =
-  case p of
-    (Less t (ConstFact c x)) -> Less (ConstFact (negateByMul c) t) x
-    p_ -> p_
+    case p of
+        Less t (ConstFact c x) ->
+            Less (ConstFact (negateByMul c) t) x
+
+        p_ ->
+            p_
+
 
 solve : Prop -> Prop
 solve p =
     case p of
-        Top -> Top
+        Top ->
+            Top
 
-        Bot -> Bot
+        Bot ->
+            Bot
 
-        Conj p1 p2 -> Conj (solve p1) (solve p2)
+        Conj p1 p2 ->
+            Conj (solve p1) (solve p2)
 
-        Disj p1 p2 -> Disj (solve p1) (solve p2)
+        Disj p1 p2 ->
+            Disj (solve p1) (solve p2)
 
-        Forall s p1 -> Forall s (solve p1)
+        Forall s p1 ->
+            Forall s (solve p1)
 
-        Exists p1 -> Exists (solve p1)
+        Exists p1 ->
+            Exists (solve p1)
 
-        Neg p1 -> Neg (solve p1) -- this case should not happen.
+        Neg p1 ->
+            Neg (solve p1)
 
-        Pred rp -> Pred (solveRatPred rp)
+        -- this case should not happen.
+        Pred rp ->
+            Pred (solveRatPred rp)
 
-        Id x -> Id x
+        Id x ->
+            Id x
