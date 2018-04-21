91b971a485508bf627eb8dcc412a181211aeaf2c
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-04-21 17:04:45 -0500

    Some progress on left and right infinite projections

--- src/InfiniteProjection.elm
+++ src/InfiniteProjection.elm
@@ -1,56 +1,111 @@
-module InfiniteProjection exposing (..)
+module InfiniteProjection exposing (leftInfProj, rightInfProj)
 
 import Syntax exposing (Prop(..), RatPred(..), Expr(..))
+import PropositionParser exposing (subst)
 
 
-leftInfProj : Prop -> Prop
-leftInfProj p =
+mkEmptyReplacedList x =
+    ( x, [] )
+
+
+type InfiniteProjectionKind
+    = LeftInfProj
+    | RightInfProj
+
+
+infProj : Prop -> InfiniteProjectionKind -> ( Prop, List Expr )
+infProj p ipk =
     case p of
         Pred (Less (Var _ _) a) ->
-            Top
+            case ipk of
+                LeftInfProj ->
+                    ( Top, [ a ] )
+
+                RightInfProj ->
+                    ( Bot, [ a ] )
 
         Pred (Less a (Var _ x)) ->
-            Bot
+            case ipk of
+                RightInfProj ->
+                    ( Top, [ a ] )
+
+                LeftInfProj ->
+                    ( Bot, [ a ] )
 
         Pred (Eq (Var _ x) c) ->
-            Bot
+            case ipk of
+                RightInfProj ->
+                    ( Bot, [ c ] )
+
+                LeftInfProj ->
+                    ( Bot, [ c ] )
 
         Pred rp ->
-            Pred rp
+            mkEmptyReplacedList (Pred rp)
 
         Id s ->
-            Id s
+            mkEmptyReplacedList (Id s)
 
         Top ->
-            Top
+            mkEmptyReplacedList Top
 
         Bot ->
-            Bot
+            mkEmptyReplacedList (Bot)
 
-        Neg p ->
-            Neg (leftInfProj p)
+        Neg p1 ->
+            let
+                ( p1LInfProj, replaced ) =
+                    infProj p1 ipk
+            in
+                ( Neg p1LInfProj, replaced )
 
         Conj p1 p2 ->
-            Conj (leftInfProj p1) (leftInfProj p2)
+            let
+                ( p1LInfProj, replaced1 ) =
+                    infProj p1 ipk
+
+                ( p2LInfProj, replaced2 ) =
+                    infProj p2 ipk
+            in
+                ( Conj p1LInfProj p2LInfProj, replaced1 ++ replaced2 )
 
         Disj p1 p2 ->
-            Disj (leftInfProj p1) (leftInfProj p2)
+            let
+                ( p1LInfProj, replaced1 ) =
+                    infProj p1 ipk
+
+                ( p2LInfProj, replaced2 ) =
+                    infProj p2 ipk
+            in
+                ( Disj p1LInfProj p2LInfProj, replaced1 ++ replaced2 )
 
         Forall vi p ->
-            Forall vi (leftInfProj p)
+            let
+                ( p1LInfProj, replaced ) =
+                    infProj p ipk
+            in
+                ( Forall vi p1LInfProj, replaced )
 
         Exists vi p ->
-            Exists vi (leftInfProj p)
+            let
+                ( p1LInfProj, replaced ) =
+                    infProj p ipk
+            in
+                ( Exists vi p1LInfProj, replaced )
+
+
+leftInfProj =
+    \x -> infProj x LeftInfProj
+
+
+rightInfProj =
+    \x -> infProj x RightInfProj
 
 
 
--- TODO
-
-
-rightInfProj : Prop -> Prop
-rightInfProj p =
-    p
-
-
-
+-- constructF3 : Prop -> Prop
+-- constructF3 p =
+-- case p of
+-- (Exists _ p1) -> (su
+-- p1 -> p1
 -- TODO
