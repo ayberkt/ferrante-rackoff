d63f9a68ae730ba8e6246d3b381e49511d5f6f76
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-04-28 23:58:31 -0500

    Begin migration to elm-mdl for UI

--- src/Main.elm
+++ src/Main.elm
@@ -1,129 +1,157 @@
 module Main exposing (..)
 
-import Html exposing (Html, Attribute, beginnerProgram, text, div, input, li, ul)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
-import String
-import Syntax exposing (Prop(..), linearize, linearizeExpr)
-import PropositionParser exposing (parseProp)
-import Styles exposing (..)
-import OmitNegations exposing (removeAllNegations)
-import Solve exposing (solve)
-import InfiniteProjection exposing (leftInfProj, rightInfProj, constructF3)
+import Html exposing (..)
+import Html.Attributes exposing (href, class, style)
+import Material
+import Material.Scheme
+import Material.Button as Button
+import Material.Grid exposing (..)
+import Material.Options as Options exposing (css)
+import Material.Textfield as Textfield
+import Material.Color as Color
+import Material.List as L
 import NNF exposing (convertToNNF)
+import Syntax exposing (..)
 
 
-main =
-    beginnerProgram { model = "", view = view, update = update }
+-- MODEL
+
+
+type Input
+    = InvalidInput
+    | Parsed Prop
+
+
+type alias Model =
+    { count : Int
+    , input : Input
+    , mdl   : Material.Model
+    }
+
+
+model : Model
+model =
+    { count = 0
+    , input = InvalidInput
+    , mdl   = Material.model
+    }
+
+
+boxed : List (Options.Property a b)
+boxed =
+    [ css "margin" "auto"
+    , css "padding-left" "8%"
+    , css "padding-right" "8%"
+    ]
 
 
 
--- UPDATE
+-- ACTION, UPDATE
 
 
 type Msg
-    = NewContent String
+    = Increase
+    | Reset
+    | UpdateMessage String
+    | Mdl (Material.Msg Msg)
 
 
-update (NewContent content) oldContent =
-    content
+
+-- Boilerplate: Msg clause for internal Mdl messages.
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increase ->
+            ( { model | count = model.count + 1 }
+            , Cmd.none
+            )
+
+        Reset ->
+            ( { model | count = 0 }
+            , Cmd.none
+            )
+
+        UpdateMessage s ->
+            ( { model | input = InvalidInput }, Cmd.none )
+
+        -- Boilerplate: Mdl action handler.
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
 
 
 
 -- VIEW
 
 
-inputInterpretation p =
-    "Input interpretation: " ++ (linearize p) ++ "."
+type alias Mdl =
+    Material.Model
 
 
-displayNNF p =
-    "Negation-normal form: " ++ linearize (convertToNNF p) ++ "."
+title : String -> Html a
+title t =
+    Options.styled Html.h1
+        [ Color.text Color.accent ]
+        [ text t ]
 
 
-displayAllNegationsRemoved p =
-    "No negations: " ++ (linearize (removeAllNegations (convertToNNF p))) ++ "."
+subTitle : String -> Html a
+subTitle t =
+    Options.styled Html.h2
+        [ Color.text Color.primary ]
+        [ text t ]
 
 
-displayConstantsSolved p =
-    "Solved constants: "
-        ++ linearize ((solve (removeAllNegations (convertToNNF p))))
-        ++ "."
+view : Model -> Html Msg
+view model =
+  Options.div
+  boxed
+    [ title "QE for Linear Rationals"
+    , Textfield.render Mdl
+        [ 2 ]
+        model.mdl
+        [ Textfield.label "Proposition" ]
+        [ Options.onInput UpdateMessage ]
+    , case model.input of
+        InvalidInput ->
+          Options.div
+            []
+            [ Options.styled Html.body
+              [ css "font-size" "20px" ]
+              [ text "Waiting for valid input" ] ]
+        Parsed p ->
+          Options.div
+          []
+          [ Button.render Mdl
+              [ 0 ]
+              model.mdl
+              [ Options.onClick Increase
+              , css "margin" "0 24px"
+              ]
+              [ text "Start" ]
+          , subTitle "Negation-normal form"
+          , Options.div
+              []
+              [ Options.styled Html.body [ css "font-size" "20px" ] [ text "Foo bar" ] ]
+          , subTitle "No negations"
+          , subTitle "Simplified"
+          ]
+    ]
+    |> Material.Scheme.top
 
 
-displayLeftInfProj p =
-    let
-        ( pLeftInfProj, replaced ) =
-            (leftInfProj (solve (removeAllNegations (convertToNNF p))))
-    in
-        "Left inf. projection: "
-            ++ linearize pLeftInfProj
-            ++ ".\n\n"
-            ++ "The following atoms have been replaced: "
-            ++ (toString (List.map linearizeExpr replaced))
+
+-- Load Google Mdl CSS. You'll likely want to do that not in code as we
+-- do here, but rather in your master .html file. See the documentation
+-- for the `Material` module for details.
 
 
-displayRightInfProj p =
-    let
-        ( pRightInfProj, replaced ) =
-            (rightInfProj (solve (removeAllNegations (convertToNNF p))))
-    in
-        "Right inf. projection: "
-            ++ linearize pRightInfProj
-            ++ ".\n\n"
-            ++ "The following atoms have been replaced: "
-            ++ (toString (List.map linearizeExpr replaced))
-
-
-displayF3 p =
-    let
-        disjuncts =
-            constructF3 (solve (removeAllNegations (convertToNNF p)))
-    in
-        (List.map (\x -> li [] [ text (linearize x) ]) disjuncts)
-
-
-
--- If the input is parsable, parse and display the steps.  If it is not
--- parsable, display an error explaining that it is not parsable.
-
-
-view content =
-    let
-        parse =
-            parseProp content
-    in
-        case parse of
-            Nothing ->
-                div [ divStyle ]
-                    [ Html.h1 [ heading ] [ text "QE for Linear Rationals" ]
-                    , input
-                        [ placeholder "Enter a proposition.", onInput NewContent, myStyle ]
-                        []
-                    , div [ waiting ] [ text ("Waiting for valid input...") ]
-                    ]
-
-            Just parse ->
-                div [ divStyle ]
-                    [ Html.h1 [ heading ] [ text "QE for Linear Rationals" ]
-                    , input
-                        [ placeholder "Enter a proposition."
-                        , onInput NewContent
-                        , myStyle
-                        ]
-                        []
-                    , Html.h2 [ heading ] [ text "Step 1" ]
-                    , div [ myStyle ] [ text (inputInterpretation parse) ]
-                    , Html.h2 [ heading ] [ text "Step 2" ]
-                    , div [ myStyle ] [ text (displayNNF parse) ]
-                    , Html.h2 [ heading ] [ text "Step 3" ]
-                    , div [ myStyle ] [ text (displayAllNegationsRemoved parse) ]
-                    , Html.h2 [ heading ] [ text "Step 4" ]
-                    , div [ myStyle ] [ text (displayConstantsSolved parse) ]
-                    , Html.h2 [ heading ] [ text "Step 5.1" ]
-                    , div [ myStyle ] [ text (displayLeftInfProj parse) ]
-                    , Html.h2 [ heading ] [ text "Step 5.2" ]
-                    , div [ myStyle ] [ text (displayRightInfProj parse) ]
-                    , Html.h2 [ heading ] [ text "Step 5.3" ]
-                    , ul [ myStyle ] (displayF3 parse)
-                    ]
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = ( model, Cmd.none )
+        , view = view
+        , subscriptions = always Sub.none
+        , update = update
+        }
