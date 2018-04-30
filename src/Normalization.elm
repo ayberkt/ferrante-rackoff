0ad0dad154bb161c3a3e8f9912ca097017c90532
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-04-30 15:42:49 -0500

    Change type of `normalizeExpr` to `Maybe Float` and rewrite all functions that use using `andThen`

--- src/Normalization.elm
+++ src/Normalization.elm
@@ -1,37 +1,71 @@
 module Normalization exposing (normalizeExpr, normalizeRatPred, normalize)
 
-import Syntax exposing (Prop(..), RatPred(..), Expr(..), Rat(..))
-import Set
 import PropositionParser exposing (subst)
+import Syntax            exposing (Prop(..), RatPred(..), Expr(..), Rat(..))
+import Maybe             exposing (andThen)
+import Set
 
 -- Evaluate an arithmetic expression to its numerical value.
-normalizeExpr : Expr -> Float
+normalizeExpr : Expr -> Maybe Float
 normalizeExpr e =
   case e of
-    Plus  e1 e2              -> normalizeExpr e1 + normalizeExpr e2
-    Minus e1 e2              -> normalizeExpr e1 - normalizeExpr e2
-    ConstRat  (Div r1 r2)    -> toFloat r1 / toFloat r2
-    ConstFact (Div r1 r2) e1 -> (toFloat r1 / toFloat r2) * normalizeExpr e1
-    Var _ _ -> -1
+    Plus  e1 e2 ->
+      normalizeExpr e1 |>
+        andThen (\e1n ->
+          normalizeExpr e2 |>
+            andThen (\e2n ->
+              Just (e1n - e2n)))
+    Minus e1 e2 ->
+      normalizeExpr e1 |>
+        andThen (\e1n ->
+          normalizeExpr e2 |>
+            andThen (\e2n ->
+              Just (e1n - e2n)))
+    ConstRat  (Div r1 r2)    -> Just (toFloat r1 / toFloat r2)
+    ConstFact (Div r1 r2) e1 ->
+      normalizeExpr e1 |>
+        andThen (\e1n -> Just ((toFloat r1 / toFloat r2) * e1n))
+    Var _ _ -> Nothing
 
 -- Evaluate a rational predicate to its Boolean value.
-normalizeRatPred : RatPred -> Bool
+normalizeRatPred : RatPred -> Maybe Bool
 normalizeRatPred rp =
   case rp of
-    Less    e1 e2 -> normalizeExpr e1 <  normalizeExpr e2
-    Greater e1 e2 -> normalizeExpr e1 >  normalizeExpr e2
-    Eq      e1 e2 -> normalizeExpr e1 == normalizeExpr e2
+    Less    e1 e2 ->
+      normalizeExpr e1
+        |> andThen (\e1n ->
+      normalizeExpr e2
+        |> andThen (\e2n -> Just (e1n < e2n)))
+    Greater e1 e2 ->
+      normalizeExpr e1
+        |> andThen (\e1n ->
+      normalizeExpr e2
+        |> andThen (\e2n -> Just (e1n > e2n)))
+    Eq e1 e2 ->
+      normalizeExpr e1
+        |> andThen (\e1n ->
+      normalizeExpr e2
+        |> andThen (\e2n -> Just (e1n == e2n)))
 
 -- Evaluate a proposition to its Boolean value.
-normalize : Prop -> Bool
+normalize : Prop -> Maybe Bool
 normalize p =
   case p of
-    Top         -> True
-    Bot         -> False
-    Neg p1      -> not (normalize p1)
-    Conj p1 p2  -> normalize p1 && normalize p2
-    Disj p1 p2  -> normalize p1 || normalize p2
+    Neg p1      ->
+      normalize p1 |> (andThen (\p1n -> Just (not p1n)))
+    Conj p1 p2  ->
+      normalize p1
+        |> (andThen (\p1n ->
+      normalize p2
+        |> (andThen (\p2n -> Just (p1n && p2n)))))
+    Disj p1 p2  ->
+      normalize p1
+        |> (andThen (\p1n ->
+      normalize p2
+        |> (andThen (\p2n -> Just (p1n || p2n)))))
+    Top         -> Just True
+    Bot         -> Just False
     Pred rp     -> normalizeRatPred rp
-    Id s        -> False
-    Exists s e1 -> False
-    Forall s e1 -> False
+    Id s        -> Just False
+    Exists s e1 -> Just False
+    Forall s e1 -> Just False
