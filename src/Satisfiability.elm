fd5978a28f28011e8c27a22a1c282026d3baef2a
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-04-29 20:34:46 -0500

    Implement `getInnerMostExistential`and make use of it

--- src/Satisfiability.elm
+++ src/Satisfiability.elm
@@ -1,10 +1,11 @@
-module Satisfiability exposing (isSat)
+module Satisfiability exposing (isSat, decideFinal)
 
-import Normalization exposing (normalize)
-import Syntax        exposing (..)
+import Normalization       exposing (normalize)
+import InfiniteProjection  exposing (leftInfProj, rightInfProj, constructF3)
+import Syntax              exposing (..)
 
-isSat : Prop -> Prop -> List Prop -> Bool
-isSat l r ps =
+decideFinal : Prop -> Prop -> List Prop -> Bool
+decideFinal l r ps =
   if (normalize l) || (normalize r) then
     True
   else
@@ -12,13 +13,52 @@ isSat l r ps =
 
 type SimplifiedProp = Prop
 
-getInnermostExistential : SimplifiedProp -> SimplifiedProp
-getInnermostExistential sp r =
-  case sp of
-    Neg  sp1     ->
-    Conj sp1 sp2 ->
-    Disj sp1 sp2 ->
-    E
+type Result =
+    NoExistentialFound
+  | Existential Prop
+  | NegatedExistential Prop
 
-isSat : SimplifiedProp -> Bool
-isSat p = False -- TODO
+getInnermostExistential : Prop -> Result -> Result
+getInnermostExistential sp lastSeen =
+  case sp of
+    Exists s sp1       ->
+      getInnermostExistential sp1 (Existential (Exists s sp1))
+    Neg (Exists s sp1) ->
+      getInnermostExistential sp1 (NegatedExistential (Exists s sp1))
+    Pred _             -> lastSeen
+    Bot                -> lastSeen
+    Top                -> lastSeen
+    Neg  sp1           -> getInnermostExistential sp1 lastSeen
+    Conj sp1 sp2       ->
+      case getInnermostExistential sp1 lastSeen of
+        NoExistentialFound -> getInnermostExistential sp2 lastSeen
+        other -> other
+    Disj sp1 sp2 ->
+      case getInnermostExistential sp1 lastSeen of
+        NoExistentialFound -> getInnermostExistential sp2 lastSeen
+        other -> other
+
+    -- The following two cases must not happen.
+    Id _ -> NoExistentialFound
+    Forall _ _ -> NoExistentialFound
+
+isSat : Prop -> Bool
+isSat sp =
+    case getInnermostExistential sp NoExistentialFound of
+      Existential sp ->
+        let
+          (leftProj,  _)  = leftInfProj sp
+          (rightProj, _)  = rightInfProj sp
+          middleCases     = constructF3 sp
+        in
+          decideFinal leftProj rightProj middleCases
+
+      NegatedExistential sp ->
+        let
+          (leftProj,  _)  = leftInfProj sp
+          (rightProj, _)  = rightInfProj sp
+          middleCases     = constructF3 sp
+        in
+          not (decideFinal leftProj rightProj middleCases)
+
+      NoExistentialFound -> False
